3d095cd0b4d4d7286c4282bef627c2ba0b06ace5
Dunja Dobrnjac <dunjadobrnjac@gmail.com>
2023-12-19 13:47:28 +0100

    Napravljeno dodavnje novog oglasa

--- src/app/new-item/new-item.component.ts
+++ src/app/new-item/new-item.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CategoryService } from '../services/category.service';
+import { AttributeService } from '../services/attribute.service';
+import { ItemService } from '../services/item.service';
+import { UserService } from '../services/user.service';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
+interface Category {
+  id: number;
+  name: string;
+  status: number;
+}
 
 interface Item {
   id: number;
@@ -15,6 +26,42 @@ interface Item {
   category_id: number;
 }
 
+interface ItemObject {
+  name: string;
+  description: string;
+  price: string;
+  location: string;
+  status: number;
+  available: number;
+  creation_date: Date;
+  user: User;
+  category: Category;
+}
+
+interface Attribute {
+  id: number;
+  name: string;
+  status: number;
+  category: Category;
+}
+
+interface Attr {
+  item: any; //dodala
+  attribute: Attribute;
+  value: string;
+}
+
+interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  username: string;
+  city: string;
+  email: string;
+  telephone: string;
+  status: number;
+}
+
 @Component({
   selector: 'app-new-item',
   templateUrl: './new-item.component.html',
@@ -22,25 +69,164 @@ interface Item {
 })
 export class NewItemComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private attributeService: AttributeService,
+    private itemService: ItemService,
+    private userService: UserService,
+    private snackbar: MatSnackBar) {
+  }
 
   newItemForm!: FormGroup; /*forma dodavanje novog artikla */
   //objekat za unos
-  newItem!: Item;
-  itemState: string = ""; //bice paadjuci pa vidjeti sta i kako
+  newItem: Item = {
+    id: -1,
+    name: "",
+    description: "",
+    price: "",
+    location: "",
+    status: -1,
+    available: 1,
+    creation_date: new Date(),
+    user_id: -1,
+    category_id: -1,
+  };
 
+  categories: Category[] = [];
+  attributes: Attribute[] = [];
+  newItemAttributes: Attr[] = [];
+
+  itemObject!: ItemObject;
 
   ngOnInit(): void {
     /*validacija kod promjene lozinke */
-
     this.newItemForm = this.formBuilder.group({
+      nameControl: ['', [Validators.required]],
+      descriptionControl: ['', [Validators.required]],
+      priceControl: ['', [Validators.required]],
+      locationControl: ['', [Validators.required]],
+      statusControl: ['', [Validators.required]],
+      categoryControl: ['', [Validators.required]],
       infoControl: ['', [Validators.required]],
     })
+
+    this.categoryService.getAllCategories().subscribe(
+      categories => {
+        this.categories = categories;
+      }
+    );
   }
 
   //klik na dugme za dodavanje
   onAddNewItem() {
     console.log(this.newItem);
+    //provjera za kategoriju i status jer je dropdown meni
+    if (this.newItem.category_id == -1) {
+      this.newItemForm.controls['categoryControl'].setErrors(Validators.required);
+    }
+    if (this.newItem.status == -1) {
+      this.newItemForm.controls['statusControl'].setErrors(Validators.required);
+    }
+
+    //  console.log(this.newItemAttributes);
+    //provjera da li su unesene vrijednosti za dodatne atribute
+    for (let attr of this.newItemAttributes) {
+      if (attr.value == "") {
+        this.newItemForm.controls['infoControl'].setErrors(Validators.required);
+      }
+    }
+
+    console.log(this.newItemForm.valid);
+    if (this.newItemForm.valid) {
+      console.log("*");
+      //sad kad je sve uneseno
+      //kreira novi artikal u tabeli item
+      let selectedCategory: Category;
+      let currentUser: User;
+
+      this.userService.getUserById(3).subscribe( //ovde treba id trenutnog korisnika
+        u => {
+          currentUser = u;
+          console.log("user -->" + currentUser.username);
+
+          this.categoryService.getCategoryById(this.newItem.category_id).subscribe(
+            cat => {
+              selectedCategory = cat;
+              console.log("category ->" + selectedCategory.name);
+
+              this.itemObject = {
+                name: this.newItem.name,
+                description: this.newItem.description,
+                price: this.newItem.price,
+                location: this.newItem.location,
+                status: this.newItem.status,
+                available: this.newItem.available,
+                creation_date: new Date(),
+                user: currentUser,
+                category: selectedCategory
+              }
+
+
+              this.itemService.addNewItem(this.itemObject).subscribe(
+                response => {
+                  console.log(response); //vrati kreirani objekat
+                  //ako je kreiran , ispisati snack poruku i obrisati sadrzaj forme
+
+                  //popunjava item_attribute tabelu sa vrijednostima
+
+                  for (let attr of this.newItemAttributes) {
+                    attr.item = response;
+                  }
+                  console.log(this.newItemAttributes);
+
+                  this.attributeService.addNewItemAttribute(this.newItemAttributes).subscribe(
+                    response => {
+                      console.log(response);
+                      if (response != null) {
+                        this.snackbar.open("Uspješno ste dodali novi oglas.", "",
+                          {
+                            duration: 4000,
+                            horizontalPosition: 'center',
+                            verticalPosition: 'top'
+                          }
+                        )
+                        this.newItemForm.reset();
+                        this.newItemForm.markAsPristine();
+                        this.newItemForm.markAsUntouched();
+                      } else {
+                        this.snackbar.open("Dodavanje novog oglasa nije uspjelo. Poušajte kasnije ponovo.", "",
+                          {
+                            duration: 4000,
+                            horizontalPosition: 'center',
+                            verticalPosition: 'top'
+                          }
+                        )
+                      }
+                    }
+                  );
+
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  }
+
+  onCategoryChanged(event: any) {
+    console.log(event.value);
+    this.newItem.category_id = event.value;
+    this.newItemAttributes = [];
+
+    this.attributeService.getCategoryAttributes(this.newItem.category_id).subscribe(
+      attr => {
+        this.attributes = attr;
+        for (const a of attr) {
+          this.newItemAttributes.push({ item: null, attribute: a, value: "" });
+        }
+      }
+    );
   }
 
 }
